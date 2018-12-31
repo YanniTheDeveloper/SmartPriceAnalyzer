7da54a4d4aabd29a9a29f2f2e3cd1f32913bf4ab
Yanni TheDeveloper <44473705+YanniTheDeveloper@users.noreply.github.com>
2018-12-31 05:27:51 -0800

    Initial commit

---
+++ app/src/main/java/ytd/smartpriceanalyzer/MainActivity.java
@@ -0,0 +1,125 @@
+package ytd.smartpriceanalyzer;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+public class MainActivity extends AppCompatActivity {
+    EditText buyR ;
+    EditText profitR ;
+    EditText shippingR;
+    EditText shippingYC ;
+    EditText otherR ;
+    EditText otherYC ;
+    EditText agentR;
+    EditText rate;
+    View baseLayout;
+    AdView advert;
+    ImageView list;
+    ImageView save;
+
+    Button analyze ;
+    TextView sellPrice ;
+
+    double buyRN = 0.0, profitRN = 0.0, shippingRN = 0.0, shippingYCN = 0.0, otherRN = 0.0, otherYCN = 0.0, agentRN = 0.0, rateN = 0.0;
+    double sumR = 0.0, sumYC = 0.0 , price = 0.0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713");
+        advert = findViewById(R.id.advert);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        advert.loadAd(adRequest);
+
+         buyR =  findViewById(R.id.buyR);
+         profitR =  findViewById(R.id.profitR);
+         shippingR =  findViewById(R.id.shippingR);
+         shippingYC =  findViewById(R.id.shippingYC);
+         otherR =  findViewById(R.id.otherR);
+         otherYC =  findViewById(R.id.otherYC);
+         agentR =  findViewById(R.id.agentR);
+        rate =  findViewById(R.id.rate);
+        analyze =  findViewById(R.id.analyze);
+        sellPrice =  findViewById(R.id.sellPrice);
+        baseLayout =  findViewById(R.id.baseLayout);
+        list = findViewById(R.id.listbtn);
+        save = findViewById(R.id.savebtn);
+
+        analyze.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resignKeyboard();
+                buyRN = getValue(buyR);
+                profitRN = getValue(profitR);
+                shippingRN = getValue(shippingR);
+                shippingYCN = getValue(shippingYC);
+                otherRN = getValue(otherR);
+                otherYCN = getValue(otherYC);
+                agentRN = getValue(agentR);
+                rateN = getValue(rate);
+
+                sumR = buyRN + profitRN + shippingRN + otherRN + agentRN;
+                sumYC = shippingYCN + otherYCN;
+                price = (sumR * rateN) + sumYC;
+
+                sellPrice.setText("You should sell it: "+price);
+            }
+
+        });
+
+        list.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toList = new Intent (MainActivity.this, MyItemList.class);
+                startActivity(toList);
+            }
+        });
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toSave = new Intent (MainActivity.this, SaveItem.class);
+                startActivity(toSave);
+            }
+        });
+
+        baseLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    resignKeyboard();
+            }
+        });
+
+    }
+
+    double getValue(EditText editText){
+        double value;
+        try{
+            value = Double.parseDouble(editText.getText().toString());
+        }catch(Exception e){
+            value = 0.0;
+        }
+        return value;
+    }
+    void resignKeyboard(){
+        try {
+            InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        }catch(Exception e){}
+    }
+}
